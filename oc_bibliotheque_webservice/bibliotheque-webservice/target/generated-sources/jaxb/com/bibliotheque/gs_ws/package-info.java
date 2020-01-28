8d4b8516a579c133486f9e2a5eb5ea6f9796e851
Laverriere <laverriere.celim@gmail.com>
2020-01-28 14:36:59 +0100

    Correction conflit merge
    -  Correction effectuée suite à une erreur d'arbitrage lors de la gestion des conflits, concernant la page HTML  "emprunt.html".

--- oc_bibliotheque_webservice/bibliotheque-webservice/target/generated-sources/jaxb/com/bibliotheque/gs_ws/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.11 
-// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2020.01.28 à 11:12:50 AM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.webservice.org/bibliotheque-ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.bibliotheque.gs_ws;
